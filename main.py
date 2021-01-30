bd040b880f0b71a67e5c348d9307e0bc30ea6e82
Kara <skizyaka@gmail.com>
2021-01-30 18:13:29 +0500

    123

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import scapy.all as sc
 
 def scan(ip):
     arp_request = sc.ARP(pdst=ip)
-    arp_request.show()
     broadcast = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered, unanswered = sc.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary)
 #Узнать значение переменной   sc.ls(sc.ARP())
 #   sc.arping(ip)
 
